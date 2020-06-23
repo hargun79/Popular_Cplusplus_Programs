0e6bbb6f4916a7bac7da0c66159a8797ea57b859
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-06-23 18:00:16 +0530

    Added new file

---
+++ depthFirstSearchUsingRecursion.cpp
@@ -0,0 +1,37 @@
+// To implement depth first search using recursion
+
+#include<bits/stdc++.h>
+using namespace std;
+
+const int maxn=2e6+10;
+int n,m;
+vector<int> vc[maxn];
+bool mark[maxn];
+
+// Function to implement depth first search
+void dfs(int v)
+{
+ cout<<v<<'\n';
+ mark[v]=1;
+ for(int u: vc[v])
+  if(!mark[u])
+   dfs(u);
+}
+
+int main()
+{
+ cin>>n>>m;
+ for(int u,v,i=0;i<m;i++)
+ {
+  cin>>u>>v;
+  u--; 
+  v--;
+  vc[u].push_back(v);
+  vc[v].push_back(u);
+ }
+ int cnt=0;
+ for(int i=0;i<n;i++)
+  if(!mark[i])
+   dfs(i);
+ return 0;  
+}
