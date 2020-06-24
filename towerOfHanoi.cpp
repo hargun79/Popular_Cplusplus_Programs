2e0be246999ac9b1b2561e03e0bb6b35caa21580
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-06-24 12:28:24 +0530

    Added new file

---
+++ towerOfHanoi.cpp
@@ -0,0 +1,24 @@
+// To implement Tower of Hanoi algorithm.
+
+#include <bits/stdc++.h> 
+using namespace std; 
+  
+// Function to implement Tower of Hanoi algorithm
+void towerOfHanoi(int n,char from_rod,char to_rod,char aux_rod)  
+{  
+ if(n==1)  
+ {  
+  cout<<"Move disk 1 from rod "<<from_rod<<" to rod "<<to_rod<<'\n';  
+  return;  
+ }  
+ towerOfHanoi(n-1,from_rod,aux_rod,to_rod);  
+ cout<<"Move disk "<<n<<" from rod "<<from_rod<<" to rod "<<to_rod<<'\n';  
+ towerOfHanoi(n-1,aux_rod,to_rod,from_rod);  
+}  
+  
+int main()  
+{  
+ int n=3;  
+ towerOfHanoi(n,'A','C','B');  
+ return 0;  
+}
