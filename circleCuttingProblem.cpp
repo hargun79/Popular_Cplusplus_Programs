b9f294f3e41a391fa451bc21dcab5e128fca5409
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-07-27 13:40:51 +0530

    Added new file

---
+++ circleCuttingProblem.cpp
@@ -0,0 +1,12 @@
+// To implement circle cutting problem
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+ unsigned long long int n;
+ cin>>n;
+ cout<<"Maximum number of pieces that can be obtained are: "<<((n*n)+n+2)/2;		 
+ return 0;
+}
