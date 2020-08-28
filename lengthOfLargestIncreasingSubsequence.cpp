6ded847715258a5cfe11e31c4861e58ec8f52eb7
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-08-28 19:16:06 +0530

    Added new file

---
+++ lengthOfLargestIncreasingSubsequence.cpp
@@ -0,0 +1,27 @@
+// To find the length of the largest increasing subsequence
+
+#include<bits/stdc++.h> 
+using namespace std; 
+	
+// Function to find the length of the largest increasing subsequence
+int lis(int arr[],int n) 
+{ 
+ int lis[n]; 
+ lis[0]=1; 
+ for(int i=1;i<n;i++) 
+ { 
+  lis[i]=1; 
+  for(int j=0;j<i;j++) 
+   if(arr[i]>arr[j] && lis[i]<lis[j]+1) 
+	lis[i]=lis[j]+1; 
+ } 
+ return *max_element(lis,lis+n); 
+} 
+	
+int main() 
+{ 
+ int arr[]={10,22,9,33,21,50,41,60}; 
+ int n=sizeof(arr)/sizeof(arr[0]); 
+ cout<<"Length of largest increasing subsequence is: "<<lis(arr,n ); 
+ return 0; 
+}
