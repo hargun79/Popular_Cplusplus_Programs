8740e536cec6cdfc05c0c839f6b8b89127b64f86
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-11-11 13:24:39 +0530

    Update biconnectedComponents.cpp

--- Graph/biconnectedComponents.cpp
+++ Graph/biconnectedComponents.cpp
@@ -1,26 +1,12 @@
-// To find biconnected components in a given graph (A biconnected component of a graph  is a connected subgraph that cannot be broken into disconnected pieces by deleting any single node)
+/* To find biconnected components in a given graph (A biconnected component of a graph is a maximally connected subgraph that cannot be broken into disconnected 
+pieces by deleting any single node)*/
  
 #include<bits/stdc++.h>
 using namespace std;
 
-typedef pair<int,int> iPair;
-
 #define NIL -1 
 
 int c=0; 
-
-class Edge 
-{ 
- public:int u; 
-        int v; 
-        Edge(int u,int v); 
-}; 
-
-Edge::Edge(int u,int v) 
-{ 
- this->u=u; 
- this->v=v; 
-}
 
 // Function to add an edge
 void addEdge(int v, int w,list<int> adj[]) 
@@ -30,7 +16,7 @@ void addEdge(int v, int w,list<int> adj[])
 }
 
 // Function to find the biconnected components
-void BCCUtil(int u,int disc[],int low[],list<Edge>* st,int parent[],list<int> adj[]) 
+void BCCUtil(int u,int disc[],int low[],list<pair<int,int>>* st,int parent[],list<int> adj[]) 
 { 
  static int time=0; 
  disc[u]=low[u]=++time; 
@@ -43,17 +29,17 @@ void BCCUtil(int u,int disc[],int low[],list<Edge>* st,int parent[],list<int> ad
   {  
    children++; 
    parent[v]=u; 
-   st->push_back(Edge(u,v)); 
+   st->push_back({u,v}); 
    BCCUtil(v,disc,low,st,parent,adj); 
    low[u]=min(low[u],low[v]); 
    if((disc[u]==1 && children>1) || (disc[u]>1 && low[v]>=disc[u])) 
    { 
-    while(st->back().u!=u || st->back().v!=v)
+    while(st->back().first!=u || st->back().second!=v)
 	{ 
-	 cout<<st->back().u<<"--"<<st->back().v<<" "; 
+	 cout<<st->back().first<<"--"<<st->back().second<<" "; 
      st->pop_back(); 
 	} 
-	cout<<st->back().u<<"--"<<st->back().v; 
+	cout<<st->back().first<<"--"<<st->back().second; 
     st->pop_back(); 
 	cout<<'\n'; 
 	c++; 
@@ -64,7 +50,7 @@ void BCCUtil(int u,int disc[],int low[],list<Edge>* st,int parent[],list<int> ad
    { 
     low[u]=min(low[u],disc[v]); 
     if(disc[v]<disc[u]) 
-	 st->push_back(Edge(u,v)); 
+	 st->push_back({u,v}); 
    } 
  } 
 } 
@@ -75,7 +61,7 @@ void BCC(int V,int E,list<int> adj[])
  int* disc=new int[V]; 
  int* low=new int[V]; 
  int* parent=new int[V]; 
- list<Edge>* st=new list<Edge>[E]; 
+ list<pair<int,int>>* st=new list<pair<int,int>>[E]; 
  for(int i=0;i<V;i++) 
  { 
   disc[i]=NIL; 
@@ -90,7 +76,7 @@ void BCC(int V,int E,list<int> adj[])
   while(st->size()>0) 
   { 
    j=1; 
-   cout<<st->back().u<<"--"<<st->back().v<<" "; 
+   cout<<st->back().first<<"--"<<st->back().second<<" "; 
    st->pop_back(); 
   } 
   if(j==1) 
@@ -115,4 +101,4 @@ int main()
  BCC(v,e,adj);
  cout<<"No of biconnected components are: "<<c;
  return 0; 
-} 
+}
