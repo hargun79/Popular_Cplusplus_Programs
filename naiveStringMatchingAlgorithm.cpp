e50e50596546a51ee42e70a33229ee93457ac958
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2020-12-06 19:06:57 +0530

    Added new files

---
+++ naiveStringMatchingAlgorithm.cpp
@@ -0,0 +1,27 @@
+// To implement naive string matching algorithm
+ 
+#include <bits/stdc++.h> 
+using namespace std; 
+
+void search(char* pat,char* txt) 
+{ 
+ int M=strlen(pat); 
+ int N=strlen(txt); 
+ for(int i=0;i<=N-M;i++) 
+ { 
+  int j; 
+  for(j=0;j<M;j++) 
+   if(txt[i+j]!=pat[j]) 
+	break; 
+  if(j==M)  
+   cout<<"Pattern found at index "<<i<<'\n'; 
+ } 
+} 
+
+int main() 
+{ 
+ char txt[]="OKOKOK...COOLCOOLCOOL"; 
+ char pat[]="OK"; 
+ search(pat,txt); 
+ return 0; 
+} 
