90f4f04bcf84f83b794ffc18a34b9d1e32eb5df4
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2021-03-02 19:58:12 +0530

    Update lengthOfLargestIncreasingSubsequence.cpp

--- lengthOfLargestIncreasingSubsequence.cpp
+++ lengthOfLargestIncreasingSubsequence.cpp
@@ -25,3 +25,15 @@ int main()
  cout<<"Length of largest increasing subsequence is: "<<lis(arr,n ); 
  return 0; 
 }
+
+/* Maximum sum LIS
+int maxSumIS(int arr[], int n)  
+{  
+ vector<int> dp(arr,arr+n);
+ for(int i=1;i<n;i++) 
+  for(int j=0;j<i;j++)
+   if(arr[j]<arr[i])
+    dp[i]=max(dp[i],arr[i]+dp[j]);
+ return *max_element(dp.begin(),dp.end());
+}
+*/	
