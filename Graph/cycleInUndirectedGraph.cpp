021c632817906dab53bc187fbc750ae09f8cfea5
Hargun Singh Sahni <hargunsinghsahni@gmail.com>
2021-07-05 00:40:04 +0530

    Create cycleInUndirectedGraph.cpp

---
+++ Graph/cycleInUndirectedGraph.cpp
@@ -0,0 +1,26 @@
+bool flag=false;
+    
+void dfs(vector<int> adj[],bool *vis,int v,int p)
+{
+ vis[v]=true;
+ for(auto u:adj[v])
+  if(!vis[u])
+   dfs(adj,vis,u,v);
+  else
+  {
+   if(u!=p)
+    flag=true;
+  }
+}
+    
+//Function to detect cycle in an undirected graph.
+bool isCycle(int V, vector<int>adj[])
+{
+ flag=false;
+ bool vis[V];
+ memset(vis,false,sizeof vis);
+ for(int i=0;i<V;i++)
+	if(!vis[i])
+	 dfs(adj,vis,i,-1);
+ return flag;
+}
